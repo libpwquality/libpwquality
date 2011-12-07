c0d1291f87055be9aa7df59ef779fcc08433247e
Tomas Mraz <tmraz@redhat.com>
2011-12-07 17:55:12 +0100

    Correct the description strings.

--- python/pwquality.c
+++ python/pwquality.c
@@ -104,17 +104,17 @@ static PyGetSetDef pwqsettings_getseters[] = {
         },
         { "maxclassrepeat",
                 (getter)pwqsettings_getint, (setter)pwqsettings_setint,
-                "Maximum repeated consecutive characters",
+                "Maximum consecutive characters of the same class",
                 (void *)PWQ_SETTING_MAX_CLASS_REPEAT
         },
         { "gecoscheck",
                 (getter)pwqsettings_getint, (setter)pwqsettings_setint,
-                "Maximum repeated consecutive characters",
+                "Match words from the passwd GECOS field if available",
                 (void *)PWQ_SETTING_GECOS_CHECK
         },
         { "badwords",
                 (getter)pwqsettings_getstr, (setter)pwqsettings_setstr,
-                "Path to the cracklib dictionary",
+                "List of words more than 3 characters long that are forbidden",
                 (void *)PWQ_SETTING_BAD_WORDS
         },
         { "dictpath",
