1fa0bd43e334d132ce199300b3d12711ce34ea18
Gabe <redhatrises@gmail.com>
2018-08-23 10:31:35 -0600

    Move retry, enforce_for_root, local_users_only options to pwquality.conf
    - retry, enforce_for_root, local_users_only options can now be configured in pwquality.conf
    - Fixes RHBZ#1537240

--- src/pwquality.h
+++ src/pwquality.h
@@ -30,6 +30,9 @@ extern "C" {
 #define PWQ_SETTING_DICT_CHECK      15
 #define PWQ_SETTING_USER_CHECK      16
 #define PWQ_SETTING_ENFORCING       17
+#define PWQ_SETTING_RETRY_TIMES     18
+#define PWQ_SETTING_ENFORCE_ROOT    19
+#define PWQ_SETTING_LOCAL_USERS     20
 
 #define PWQ_MAX_ENTROPY_BITS       256
 #define PWQ_MIN_ENTROPY_BITS       56
